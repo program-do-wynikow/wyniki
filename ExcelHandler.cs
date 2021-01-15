7f7d01a8bc7e0cc3795f8eac2f16daa2fd42a65b
bartus12k <77514377+bartus12k@users.noreply.github.com>
2021-01-15 23:23:30 +0100

    Add files via upload

---
+++ ExcelHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Text;
+
+namespace WIZUALIZACJA_CAT_STREAM
+{
+    class ExcelHandler
+    {
+        public DataTable data { get; set; }
+        public OleDbDataAdapter adapter { get; set; }
+        public DataSet ds { get; set; }
+
+        public ExcelHandler()
+        {
+            data = null;
+            adapter = null;
+            ds = null;
+        }
+        public ExcelHandler(string fileName, string spreadsheet)
+        {
+            string connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0; data source={0}; Extended Properties=Excel 8.0;", fileName);
+            adapter = new OleDbDataAdapter($"SELECT * FROM [{spreadsheet}$]", connectionString);
+            ds = new DataSet();
+            adapter.Fill(ds, "A");
+            data = ds.Tables["A"];
+        }
+        public string Row_toString(int row)
+        {
+            string res = "";
+            //int j = 0;
+            //while (data.Rows[row][j].ToString() != null) res += 
+            for (int j = 0; j < data.Columns.Count; j++) res += data.Rows[row][j].ToString() + " ";
+            return res;
+        }
+    }
+}
